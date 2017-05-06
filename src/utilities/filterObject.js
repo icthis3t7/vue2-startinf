fb15d2b3c797bb7c9a4eebd4c53b6824d15d7735
icthis3t7 <icthis3t7@gmail.com>
2017-05-06 15:52:46 -0500

    operations on objects
    return only matching properties

---
+++ src/utilities/filterObject.js
@@ -0,0 +1,29 @@
+// perform operations on JS objects
+export default {
+  byProperty: function (objectToFilter, propertyToFilterOn, filterValue) {
+    // ensure data is returned if no provided filter filterValue
+    if (filterValue) {
+      let filtered = {};
+
+      // perform the filter
+      for (var key in objectToFilter) {
+        let singleObjectItem = objectToFilter[key];
+        debugger;
+
+        if (singleObjectItem && singleObjectItem[propertyToFilterOn] && singleObjectItem[propertyToFilterOn].toLowerCase().indexOf(filterValue) > -1) {
+          filtered[key] = singleObjectItem;
+
+        }
+
+      }
+
+      return filtered;
+
+    } else {
+      return objectToFilter;
+
+    }
+
+  }
+
+}
