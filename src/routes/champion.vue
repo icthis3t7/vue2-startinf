449e2d2afdcb0362a8c7473dffdfbbb5943fedb4
icthis3t7 <icthis3t7@gmail.com>
2017-05-06 15:57:06 -0500

    updated code formatting

--- src/routes/champion.vue
+++ src/routes/champion.vue
@@ -9,20 +9,18 @@
 
     </div>
 
-    <input v-model="filterName" placeholder="Champion Name" style="float: left;">
+    <input v-if="champs" v-model="filterName" placeholder="Champion Name" style="float: left;">
 
     <br />
     <br />
     <br />
 
-    <div v-if="champs" v-for="champ in filterByProperty(champs, 'name', filterName)" style="float: left;">
+    <div v-if="champs" v-for="champ in filterObjectByProperty(champs, 'name', filterName)" style="float: left;">
       <img :src="'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + champ.key + '_0.jpg'" />
 
       <p>Name: {{ champ.name }} - {{ champ.title }}</p>
 
     </div>
-
-
 
     <p :if="error">{{ error }}</p>
 
@@ -33,6 +31,7 @@
 
 <script>
 import champion from '@/api/champion/champion';
+import filterObject from '@/utilities/filterObject';
 
 
 export default {
@@ -51,46 +50,21 @@ export default {
 
   created () {
     // get all champ data (done every time the vue is loaded)
-    var _self = this;
+    let _self = this;
 
-    champion.getAllChampionStaticData().then(function (champs) {
+    champion.getAllChampionStaticData().then(champs => {
       _self.champs = champs;
 
-    }, function (error) {
+    }, error => {
       _self.error = error;
 
     });
 
   },
 
+  //TODO: see if we can just import the method....
   methods: {
-    filterByProperty: function (data, prop, value) {
-      if (value) {
-        let filtered = {};
-
-        for (var key in data) {
-          console.log('string to filter on: ' + data[key][prop]);
-          console.log('value filtering it : ' + value);
-
-          console.log(data[key][prop][0].toLowerCase() + ' \ ' + value[0].toLowerCase());
-          console.log(data[key][prop].toLowerCase().indexOf(value));
-
-
-          if (data[key] && data[key][prop] && data[key][prop][0].toLowerCase() === value[0].toLowerCase() && data[key][prop].toLowerCase().indexOf(value) > -1) {
-            filtered[key] = data[key];
-
-          }
-
-        }
-
-        return filtered;
-
-      } else {
-        return data;
-
-      }
-
-    }
+    filterObjectByProperty: filterObject.byProperty
 
   }
 
