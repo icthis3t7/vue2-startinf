959b907bec59203158f278baa038518daa14e513
icthis3t7 <icthis3t7@gmail.com>
2017-05-06 15:16:58 -0500

    updated endpoint

--- src/api/champion/champion.js
+++ src/api/champion/champion.js
@@ -1,8 +1,11 @@
 //TODO: look into a different promise library (focus on performance)
 //TODO: consider a different ajax library (is one even needed?)
 
-import cache from '@/api/cache/cache';
 import axios from 'axios';
+
+import keys from '@/_keys/keys.js';
+import cache from '@/api/cache/cache';
+
 
 export default {
   // get all data provided by riot static endpoint
@@ -18,8 +21,9 @@ export default {
       } else {
         // get data from riot then cache
         //TODO: remove key from client
-        axios.get('https://na1.api.riotgames.com/lol/static-data/v3/champions?champListData=info')
+        axios.get('https://na1.api.riotgames.com/lol/static-data/v3/champions?champListData=info&api_key=' + keys.riot)
         .then(response => {
+          debugger;
           // ensure valid data
           if (response && response['data'] && response.data['data']) {
             cache.set('allChampions', response.data.data);
