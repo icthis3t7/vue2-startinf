c41278ef14cbeae0c754b8e4c65c49ec8e775073
icthis3t7 <icthis3t7@gmail.com>
2017-05-06 22:18:33 -0500

    single champion vue (loads riot data)

--- src/api/champion/champion.js
+++ src/api/champion/champion.js
@@ -1,4 +1,4 @@
-//TODO: look into a different promise library (focus on performance)
+//TODO: BIG ISSUE look into a different promise library (focus on performance)
 //TODO: consider a different ajax library (is one even needed?)
 import axios from 'axios';
 
@@ -8,6 +8,9 @@ import sortObject from '@/utilities/sortObject';
 
 
 export default {
+  // props
+  keys: keys,
+
   // get all data provided by riot static endpoint
   getAllChampionStaticData: function() {
     let _self = this;
@@ -23,7 +26,7 @@ export default {
       } else {
         // get data from riot then cache
         //TODO: remove key from client
-        axios.get('https://na1.api.riotgames.com/lol/static-data/v3/champions?champListData=info&api_key=' + keys.riot)
+        axios.get('https://na1.api.riotgames.com/lol/static-data/v3/champions?champListData=info&api_key=' + _self.keys.riot)
         .then(response => {
           // ensure valid data
           if (response && response['data'] && response.data['data']) {
@@ -48,6 +51,80 @@ export default {
 
     });
 
-  }
+  },
+
+
+  // single champ
+  getChampion: function (name) {
+    let _self = this;
+
+    return new Promise((resolve, reject) => {
+      let championData = cache.get(name);
+
+      if (championData) {
+        // return cached data
+        resolve(championData);
+
+      } else {
+        _self._getChampionId(name).then(id => {
+          // get data from riot then cache
+          //TODO: remove key from client
+          axios.get('https://na1.api.riotgames.com/lol/static-data/v3/champions/' + id + '?champData=all&api_key=' + _self.keys.riot)
+          .then(response => {
+            // ensure valid data
+            if (response && response['data'] ) {
+              let championData = response.data;
+
+              cache.set(name, championData);
+
+              resolve(championData);
+
+            } else {
+              reject('Valid call, data was error');
+
+            }
+
+          })
+          .catch(e => {
+            reject(e.toString);
+
+          });
+
+        }, error => {
+          reject(error);
+
+        });
+
+      }
+
+    });
+
+  },
+
+
+  // map name to id
+  _getChampionId: function (name) {
+    let _self = this;
+
+    return new Promise((resolve, reject) => {
+      _self.getAllChampionStaticData().then(allChampions => {
+        let champion = allChampions[name];
+
+        if (champion) {
+          resolve(champion.id);
+
+        } else {
+          reject('Not found');
+
+        }
+
+      }, error => {
+        reject(error.toString());
+
+      });
+
+    });
+
+  },
 
 }
--- src/routes/champion.vue
+++ src/routes/champion.vue
@@ -1,28 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-
-    <h2>Champions</h2>
-
-    <div v-if="!champs">
-      Loading...
-
-    </div>
-
-    <input v-if="champs" v-model="filterName" placeholder="Champion Name" style="float: left;">
-
-    <br />
-    <br />
-    <br />
-
-    <div v-if="champs" v-for="champ in filterObjectByProperty(champs, 'name', filterName)" style="float: left;">
-      <img :src="'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + champ.key + '_0.jpg'" />
-
-      <p>Name: {{ champ.name }} - {{ champ.title }}</p>
-
-    </div>
-
-    <p :if="error">{{ error }}</p>
+    <img :src="'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + champion.key + '_0.jpg'" />
+    <h3>{{ champion.name }}, {{ champion.title}}</h3>
+    <p>{{ champion.lore }}</p>
 
   </div>
 
@@ -31,18 +11,13 @@
 
 <script>
 import champion from '@/api/champion/champion';
-import filterObject from '@/utilities/filterObject';
-
 
 export default {
   name: 'champion',
 
   data () {
     return {
-      msg: 'Welcome to the Champion component',
-      champs: undefined,
-      error: undefined,
-      filterName: null
+      champion: undefined
 
     }
 
@@ -50,23 +25,15 @@ export default {
 
 
   created () {
-    // get all champ data (done every time the vue is loaded)
     let _self = this;
 
-    champion.getAllChampionStaticData().then(champs => {
-      _self.champs = champs;
+    champion.getChampion(_self.$route.params.name).then(champs => {
+      _self.champion = champs;
 
     }, error => {
       _self.error = error;
 
     });
-
-  },
-  
-
-  //TODO: see if we can just import the method....
-  methods: {
-    filterObjectByProperty: filterObject.byProperty
 
   }
 
